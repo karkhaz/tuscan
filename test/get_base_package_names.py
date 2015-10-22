248ecd3217cc57a010a1fa481b97532322cfcd79
Kareem Khazem <khazem@google.com>
2015-10-22 12:12:16 -0700

    Tests are more descriptive when they fail
    This commit adds docstrings to each test case, as well as code to ensure
    that the docstring is printed when the test fails.

--- test/get_base_package_names.py
+++ test/get_base_package_names.py
@@ -21,34 +21,57 @@ from os.path import exists
 from subprocess import run, DEVNULL
 from sys import path
 
+from unittest.runner import TextTestResult
+TextTestResult.getDescription = lambda _, test: test.shortDescription()
+
 class TestGetBasePackageNames(unittest.TestCase):
 
+    def shortDescription(self):
+        return self._testMethodDoc
+
     def test_shared_data_exists(self):
+        """Something is horribly wrong---the directory for shared data
+        does not exist."""
         self.assertTrue(
             exists("/tuscan_data/"))
 
     def test_top_level_exists(self):
+        """The get_base_package_names container did not create a
+        'get_base_package_names' results directory."""
         self.assertTrue(
             exists("/tuscan_data/get_base_package_names/"))
 
     def test_latest_exists(self):
+        """The get_base_package_names container did not create a
+        'latest' results directory"""
         self.assertTrue(
             exists("/tuscan_data/get_base_package_names/latest/"))
 
     def test_names_file_exists(self):
+        """The get_base_package_names container did not create a
+        file containing base package names"""
         self.assertTrue(
             exists("/tuscan_data/get_base_package_names/latest/names.py"))
 
     def test_can_import_names(self):
+        """It was not possible to import base package names from the
+        expected location."""
         path.insert(0, "/tuscan_data/get_base_package_names/latest/")
         from names import base
         from names import base_devel
 
     def test_names_are_packages(self):
+        """Each of the package names written by get_base_package_names
+        should indeed be the name of a package."""
         path.insert(0, "/tuscan_data/get_base_package_names/latest/")
         from names import base
         from names import base_devel
         for pack in base:
+            ret = run(["pacman", "--query", "--info", pack],
+                      stdout=DEVNULL, universal_newlines=True)
+            self.assertEqual([pack, ret.returncode],
+                             [pack, 0])
+        for pack in base_devel:
             ret = run(["pacman", "--query", "--info", pack],
                       stdout=DEVNULL, universal_newlines=True)
             self.assertEqual([pack, ret.returncode],
