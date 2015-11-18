cdce210ce5ed19013239926575f8854017fd1ae5
Kareem Khazem <khazem@google.com>
2015-11-18 12:00:25 -0800

    Hybrid package creation
    This commit enables the creation of hybrid packages---packages
    containing both vanilla and toolchain-built data.
    
    New stages
    ----------
    
    The create_toolchain_repo stage is added. This creates an empty
    package repository in a data container. This package repository is used
    by the make_package stage:
      * when a package is successfully built, it is added to the package
        repository;
      * dependencies of a build are installed from the package repository
        before the build begins.
    
    The install_bootstrap stage is added. This installs all vanilla packages
    that are necessary to build all other packages (the bootstrap packages).
    This container is committed after it finishes running, so that
    containers running the make_package stage can use it as a base image.
    This stage is run just once, since installing all the bootstrap packages
    is a time-consuming process.
    
    Local Arch Mirror
    -----------------
    
    Packages are now synced from a local arch mirror, rather than upstream.
    This saves bandwidth and is more polite, as well as making the
    experiment reproducible.
    
    Hybrid package creation
    -----------------------
    
    The make_package and create_toolchain_repo stages now create hybrid
    packages. When a package is built, the hybrid packages that it depends
    on are installed, and the compiler uses toolchain-built libraries and
    headers.
    
    Logging
    -------
    
    The make_package, install_bootstrap and create_toolchain_repo stages
    emits JSON-structured logs that can be parsed into HTML reports.
    
    Bugfix: file exists in filesystem
    
    Before this commit, the toolchain portion of hybrid packages would be
    installed in a global location and not uninstalled. However, the package
    database is local to each container. This means that containers
    attempting to install a dependency whose files already existed on the
    file system would fail.
    
    This commit makes the installation location of toolchain files local to
    each container, while keeping the toolchain archive global.
    
    Compiler wrapper
    ----------------
    
    This commit introduces a rudimentary compiler wrapper that replaces the
    native GCC, ld and ar programs. It prints out the name of the program it
    is replacing and then returns non-zero.
    
    This is so that we can detect when toolchain builds are ignoring our
    setting of CC and hardcoding the name of the native compiler.

--- package_build_wrapper.py
+++ package_build_wrapper.py
@@ -16,8 +16,10 @@
 # limitations under the License.
 
 from argparse import ArgumentParser
-from json import dumps, load
-from re import compile, match
+from json import dumps, load, loads
+from os import getcwd
+from os.path import basename, splitext
+from re import compile, match, sub
 from subprocess import Popen, PIPE, STDOUT
 from sys import stderr
 from time import gmtime, mktime
@@ -29,35 +31,56 @@ def get_parser():
 
     parser.add_argument("--shared-directory", dest="shared_directory",
                         action="store", required=True)
-    parser.add_argument("--shared-volume", dest="data_volume",
+    parser.add_argument("--shared-volume", dest="shared_volume",
                         action="store", required=True)
+
     parser.add_argument("--sources-directory", dest="sources_directory",
                         action="store", required=True)
     parser.add_argument("--sources-volume", dest="sources_volume",
                         action="store", required=True)
-    parser.add_argument("--target-package", dest="target_package",
+
+    parser.add_argument("--toolchain-directory",
+                        dest="toolchain_directory",
                         action="store", required=True)
-    parser.add_argument("--pkg-cache-volume", dest="pkg_cache_volume",
+    parser.add_argument("--toolchain-volume",
+                        dest="toolchain_volume",
                         action="store", required=True)
+
+    parser.add_argument("--toolchain",
+                        dest="toolchain",
+                        action="store", required=True)
+
+    parser.add_argument("--abs-dir", dest="abs_dir",
+                        action="store", required=True)
+
     parser.add_argument("--output-directory", dest="output_directory",
                         action="store", required=True)
+
     parser.add_argument("output_packages", action="store", nargs="+")
     return parser
 
 
 def run_container(args):
-    stderr.write("Running container for " + args.target_package)
     start_time = mktime(gmtime())
-    command = ("docker run -v " + args.shared_directory
-               + " --volumes-from " + args.data_volume
+
+    command = ("docker run --rm -v " + args.shared_directory
+               + " --volumes-from " + args.shared_volume
+
                + " -v " + args.sources_directory
                + " --volumes-from " + args.sources_volume
-               + " -v /var/cache/pacman/pkg"
-               + " --volumes-from " + args.pkg_cache_volume
-               + " -v logs:/logs make_package"
+
+               + (" -v %s/mirror:/mirror:ro" % getcwd())
+
+               + " -v " + args.toolchain_directory
+               + " --volumes-from " + args.toolchain_volume
+
+               + " make_package"
+
                + " --sources-directory " + args.sources_directory
                + " --shared-directory "  + args.shared_directory
-               + " " + args.target_package)
+               + " --toolchain-directory "  + args.toolchain_directory
+               + " --abs-dir " + args.abs_dir
+               + " --toolchain "  + args.toolchain)
     p = Popen(command.split(), universal_newlines=True, stdout=PIPE,
               stderr=STDOUT)
     out, _ = p.communicate()
@@ -65,9 +88,32 @@ def run_container(args):
 
     json_result = {}
     json_result["return_code"] = p.returncode
-    json_result["log"] = [command] + out.splitlines()
-    json_result["package"] = args.target_package
+    json_result["log"] = []
+
+    # The log() method in utilities.py emits a JSON dictionary rather
+    # than a string of plain text. Thus, we should try to parse each
+    # line of the log into a dictionary.
+    #
+    # If an exception was thrown during one of the stages (i.e. a
+    # problem with the stage itself rather than an external command),
+    # then the stack trace will obviously not be in JSON format, so add
+    # the raw lines to a separate array.
+    errors = []
+    for struct in out.splitlines():
+        try:
+            json_result["log"].append(loads(struct))
+        except:
+            errors.append(str(struct))
+
+    if errors:
+        stderr.write("Stage error during build of %s\n" % args.abs_dir)
+        for line in errors:
+            stderr.write(line + "\n")
+
+    json_result["package"] = args.abs_dir
     json_result["time"] = int(mktime(gmtime()) - start_time)
+    json_result["toolchain"] = args.toolchain
+    json_result["errors"] = errors
 
     for touch_file in args.output_packages:
         with open(touch_file, "w") as f:
